66afe0cea427f13b1e1cfb5828c525887ba9f1f3
Martin Robinson <mrobinson@igalia.com>
2023-01-09 16:12:32 +0100

    Set up for testing in this repository

--- etc/ci/upstream-wpt-changes/upstream-wpt-changes.py
+++ etc/ci/upstream-wpt-changes/upstream-wpt-changes.py
@@ -25,15 +25,15 @@ def main() -> int:
     logging.getLogger().level = logging.INFO
 
     success = WPTSync(
-        servo_repo='servo/servo',
-        wpt_repo='servo/wpt',
-        downstream_wpt_repo='servo-wpt-sync/web-platform-tests',
+        servo_repo='mrobinson/servo',
+        wpt_repo='mrobinson/wpt',
+        downstream_wpt_repo='mrobinson/wpt',
         servo_path='./servo',
         wpt_path='./wpt',
-        github_api_token=os.environ['WPT_SYNC_GITHUB_TOKEN'],
+        github_api_token=os.environ['GITHUB_TOKEN'],
         github_api_url='https://api.github.com/',
-        github_username='servo-wpt-sync',
-        github_email='josh+wptsync@joshmatthews.net',
+        github_username='mrobinson',
+        github_email='mrobinson@igalia.com',
         github_name='Servo WPT Sync',
     ).run(context["event"])
     return 0 if success else 1
